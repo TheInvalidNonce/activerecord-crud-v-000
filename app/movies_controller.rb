99ab2aa164f79e568db67d4bac8a74e6c74ef45e
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-13 20:43:29 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    m.title = "Sausage Party"
-    m.release_date = 2016
+    m.title = "Home alone"
+    m.release_date = 1990
   end
 end
 
