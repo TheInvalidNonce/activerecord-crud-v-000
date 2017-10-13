b01e05ed5681ff28171d1a398bea0b4c840bb26b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-13 20:45:59 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block
 end
 
 def can_get_the_first_item_in_the_database
-  self.find_by(:id => 1)
+  Movie.find_by(:id => 1)
 end
 
 def can_get_the_last_item_in_the_database
